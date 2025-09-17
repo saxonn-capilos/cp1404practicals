9ada505db913d0b1cabe19586ba2ad2d788efde9
Saxonn <saxonncapilos1@gmail.com>
2025-09-17 15:36:28 +1000

    Refactor former password code into functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,5 +1,19 @@
-password = input("Enter your password: ")
-while len(password) < 5:
-    print("Password needs to be at least 5 characters long")
+def main():
+    password = get_password()
+    while len(password) < 5:
+        print("Password needs to be at least 5 characters long")
+        password = get_password()
+    print_password(password)
+
+
+def print_password(password):
+    print("*" * len(password))
+
+
+def get_password():
     password = input("Enter your password: ")
-print("*" * len(password))
+    return password
+
+
+main()
+
