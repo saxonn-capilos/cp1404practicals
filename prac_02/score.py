e81edd140d3c670556ac9cf0e2f6a8140676cf7b
Saxonn <saxonncapilos1@gmail.com>
2025-09-22 14:37:59 +1000

    Add code to the main function that generates a random score

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,16 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
+import random
 
 
 def main():
     score = float(input("Enter score: "))
     result = determine_score(score)
     print(f"Your result is {result}")
+    random_score = random.randint(1, 100)
+    print(f"Here's a random score between 0 and 100: {random_score}")
+    print(f"It's result is {determine_score(random_score)}")
 
 
 def determine_score(score):
