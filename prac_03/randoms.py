5f6c7dd6f413a8e68e9a466e9990fc9e536fcf0f
Saxonn <saxonncapilos1@gmail.com>
2025-09-24 15:45:09 +1000

    Answer the questions and write a program to produce a random number between 1 and 100 inclusive.

---
+++ prac_03/randoms.py
@@ -0,0 +1,6 @@
+# On line 1 I saw 20. The smallest number I could've seen was 5 and the largest number I could have seen was 20.
+# On line 2 I saw 5. The smallest number I could have seen was 3 and the largest number I could have seen was 9.
+# On line 3 I saw 5.224452113996168. The smallest number I could have seen was 2.5 and the largest number I could have
+# seen was 5.5.
+
+print(random.randint(1, 100))
