cdafa33c8a571a605172f895d4854c0c1f907ffe
Saxonn <saxonncapilos1@gmail.com>
2025-09-30 13:05:02 +1000

    Write code to answer question 2.

--- prac_03/files.py
+++ prac_03/files.py
@@ -7,3 +7,10 @@ name = input("Enter your name: ")
 out_file = open("name.txt", "w")
 print(name, file=out_file)
 out_file.close()
+
+# 2.
+in_file = open("name.txt", "r")
+name = name.rstrip()
+print(f"Hi {name}!")
+in_file.close()
+
