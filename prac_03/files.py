536fd70d01141fa503466bc2c1827840ab6743d0
Saxonn <saxonncapilos1@gmail.com>
2025-09-30 13:41:16 +1000

    Write code to question 3

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,14 +3,21 @@ Do-from-scratch Exercise: Files
 """
 
 # 1.
-name = input("Enter your name: ")
-out_file = open("name.txt", "w")
-print(name, file=out_file)
-out_file.close()
+# name = input("Enter your name: ")
+# out_file = open("name.txt", "w")
+# print(name, file=out_file)
+# out_file.close()
 
 # 2.
-in_file = open("name.txt", "r")
-name = name.rstrip()
-print(f"Hi {name}!")
-in_file.close()
+# in_file = open("name.txt", "r")
+# name = name.rstrip()
+# print(f"Hi {name}!")
+# in_file.close()
+
+# 3.
+with open("numbers.txt") as in_file:
+    first_number = int(in_file.readline())
+    second_number = int(in_file.readline())
+print(first_number + second_number)
+
 
