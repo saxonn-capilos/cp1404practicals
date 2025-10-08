f57c62b691d265c585047bd24b9f1a54440604cf
Saxonn <saxonncapilos1@gmail.com>
2025-10-08 15:22:54 +1000

    Add if else statement for username input

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -22,3 +22,8 @@ Intermediate exercises
 # Woefully inadequate security checker
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input('Enter your username: ')
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
