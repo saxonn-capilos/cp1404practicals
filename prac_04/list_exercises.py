39b42d2718259bf75a7deb8e202335f50c919cfc
Saxonn <saxonncapilos1@gmail.com>
2025-10-08 15:21:22 +1000

    Add usernames list from Github

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,19 +1,24 @@
 """
 CP1404 prac_04
-Intermediate exercises: Basic list operations
+Intermediate exercises
 """
 
 
-numbers = []
-for i in range(5):
-    numbers.append(int(input("Enter a number: ")))
-first_number = numbers[0]
-last_number = numbers[-1]
-smallest_number = min(numbers)
-largest_number = max(numbers)
-average = sum(numbers) / len(numbers)
-print(f"The first number is {first_number}")
-print(f"The last number is {last_number}")
-print(f"The smallest number is {smallest_number}")
-print(f"The largest number is {largest_number}")
-print(f"The average of the numbers is {average}")
+# Basic list operations
+# numbers = []
+# for i in range(5):
+#     numbers.append(int(input("Enter a number: ")))
+# first_number = numbers[0]
+# last_number = numbers[-1]
+# smallest_number = min(numbers)
+# largest_number = max(numbers)
+# average = sum(numbers) / len(numbers)
+# print(f"The first number is {first_number}")
+# print(f"The last number is {last_number}")
+# print(f"The smallest number is {smallest_number}")
+# print(f"The largest number is {largest_number}")
+# print(f"The average of the numbers is {average}")
+
+# Woefully inadequate security checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
