6450998282ab17d9ec172effd15df86ede9aa6d3
Saxonn <saxonncapilos1@gmail.com>
2025-10-12 09:06:25 +1000

    Add code for "Quick Pick" Lottery Ticket Generator

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,5 +1,23 @@
 """
 CP1404 prac_04
-Do-from-scratch Exercsises: "Quick Pick" Lottery Ticket Generator
+Do-from-scratch Exercises: "Quick Pick" Lottery Ticket Generator
 """
 
+
+import random
+
+NUMBERS_PER_ROW = 6
+MIN_NUMBER = 1
+MAX_NUMBER = 45
+WIDTH = len(str(MAX_NUMBER))
+
+number_quick_picks = int(input("How many quick picks? "))
+for i in range(number_quick_picks):
+    quick_pick = []
+    while len(quick_pick) < NUMBERS_PER_ROW:
+        number = random.randint(MIN_NUMBER, MAX_NUMBER)
+        if number not in quick_pick:
+            quick_pick.append(number)
+    quick_pick.sort()
+
+    print(' '.join(f"{num:>{WIDTH}}" for num in quick_pick))
