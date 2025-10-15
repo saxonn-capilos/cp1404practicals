3639c36606d1b0e00915e93a79f465c98070b0b9
Saxonn <saxonncapilos1@gmail.com>
2025-10-15 13:49:56 +1000

    Change program to accept lowercase inputs

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -11,8 +11,8 @@ print(CODE_TO_NAME)
 
 state_code = input("Enter short state: ")
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
+    if state_code.upper() in CODE_TO_NAME:
+        print(state_code, "is", CODE_TO_NAME[state_code.upper()])
     else:
         print("Invalid short state")
     state_code = input("Enter short state: ")
