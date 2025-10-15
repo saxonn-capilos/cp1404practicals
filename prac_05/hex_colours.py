180526a6555c89055fd57660062248bbc6e6aeb2
Saxonn <saxonncapilos1@gmail.com>
2025-10-15 14:24:06 +1000

    Change code to ask user for colour name and print hexadecimal code instead of the other way around

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -3,16 +3,26 @@ CP1404/CP5632 Practical
 Intermediate Exercises - hexadecimal colour code program
 """
 
-CODE_TO_NAME = {"#FF0000": 'Red', '#0000FF': 'Blue', '#00FF00': 'Green', '#FFFF00': "Yellow", '#800080': "Purple",
-                '#FFA500': "Orange", '#FFC0CB': "Pink", '#008080': "Teal", '#000080': "Navy", '#FF7F50': "Coral"}
+CODE_TO_NAME = {
+'Red': '#FF0000',
+'Blue': '#0000FF',
+'Green': '#00FF00',
+'Yellow': '#FFFF00',
+'Purple': '#800080',
+'Orange': '#FFA500',
+'Pink': '#FFC0CB',
+'Teal': '#008080',
+'Navy': '#000080',
+'Coral': '#FF7F50',
+}
 
-for short_name, full_name in CODE_TO_NAME.items():
-    print(f"{short_name} is {full_name}")
+for colour_name, hexadecimal_name in CODE_TO_NAME.items():
+    print(f"{colour_name} is {hexadecimal_name}")
 
-state_code = input("Enter hexadecimal code: ")
-while state_code != "":
+colour_name = input("Enter colour: ").title()
+while colour_name != "":
     try:
-        print(state_code, "is", CODE_TO_NAME[state_code.upper()])
+        print(colour_name, "is", CODE_TO_NAME[colour_name])
     except KeyError:
-        print("Invalid hexadecimal code")
-    state_code = input("Enter hexadecimal code: ")
+        print("Invalid colour")
+    state_code = input("Enter colour: ")
