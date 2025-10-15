74c81a84ab59849b84069eaadc64a0a5093bdbf8
Saxonn <saxonncapilos1@gmail.com>
2025-10-15 15:33:57 +1000

    Add code for Word Occurrences exercise

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -1,8 +1,15 @@
 """
 Word Occurences
 Estimate: 30 minutes
-Actual: started 3:04
+Actual: 29 minutes
 """
 
-sentence = input(("Text: "))
-words = [t]
+sentence = "this is a collection of words of nice words this is a fun thing it is"
+words = sentence.split()
+word_to_count = dict.fromkeys(words, 0)
+for word in words:
+    word_to_count[word] = word_to_count.get(word, 0) + 1
+word_to_count = dict(sorted(word_to_count.items()))
+longest_word = max(len(word) for word in word_to_count)
+for word, count in word_to_count.items():
+    print(f"{word:{longest_word}} : {count}")
