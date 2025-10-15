d8a05de661a1054635ec9d0fab99e5ff4a0e565b
Saxonn <saxonncapilos1@gmail.com>
2025-10-15 15:07:22 +1000

    Fix error in while loop

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -3,26 +3,16 @@ CP1404/CP5632 Practical
 Intermediate Exercises - hexadecimal colour code program
 """
 
-CODE_TO_NAME = {
-'Red': '#FF0000',
-'Blue': '#0000FF',
-'Green': '#00FF00',
-'Yellow': '#FFFF00',
-'Purple': '#800080',
-'Orange': '#FFA500',
-'Pink': '#FFC0CB',
-'Teal': '#008080',
-'Navy': '#000080',
-'Coral': '#FF7F50',
-}
+COLOUR_TO_CODE = {"Red": "#FF0000", "Blue": "#0000FF", "Green": "#00FF00", "Yellow": "#FFFF00", "Purple": "#800080",
+                "Orange": "#FFA500", "Pink": "#FFC0CB", "Teal": "#008080", "Navy": "#000080", "Coral": "#FF7F50"}
 
-for colour_name, hexadecimal_name in CODE_TO_NAME.items():
-    print(f"{colour_name} is {hexadecimal_name}")
+for name, hexadecimal_name in COLOUR_TO_CODE.items():
+    print(f"{name} is {hexadecimal_name}")
 
-colour_name = input("Enter colour: ").title()
+colour_name = input("Enter colour: ")
 while colour_name != "":
     try:
-        print(colour_name, "is", CODE_TO_NAME[colour_name])
+        print(colour_name, "is", COLOUR_TO_CODE[colour_name.title()])
     except KeyError:
         print("Invalid colour")
-    state_code = input("Enter colour: ")
+    colour_name = input("Enter colour: ")
---
+++ prac_05/word_occurences.py
@@ -0,0 +1,8 @@
+"""
+Word Occurences
+Estimate: 30 minutes
+Actual: started 3:04
+"""
+
+sentence = input(("Text: "))
+words = [t]
