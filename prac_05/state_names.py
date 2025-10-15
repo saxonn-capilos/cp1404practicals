79403c3c4a7fef5f8d8d71a51115d699304f367f
Saxonn <saxonncapilos1@gmail.com>
2025-10-15 13:59:22 +1000

    Changed if else decision structure to use exceptions instead

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -12,8 +12,8 @@ for short_name, full_name in CODE_TO_NAME.items():
 
 state_code = input("Enter short state: ")
 while state_code != "":
-    if state_code.upper() in CODE_TO_NAME:
+    try:
         print(state_code, "is", CODE_TO_NAME[state_code.upper()])
-    else:
+    except KeyError:
         print("Invalid short state")
     state_code = input("Enter short state: ")
