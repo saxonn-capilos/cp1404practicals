073343e09e5c3eda93cf1bc01c409b487f4611e9
Saxonn <saxonncapilos1@gmail.com>
2025-10-29 15:56:14 +1000

    Start code for languages.py and programming_language.py

---
+++ prac_06/languages.py
@@ -0,0 +1,18 @@
+"""
+CP1404/CP5632 Practical
+Intermediate Exercise
+Estimated time: 35 minutes
+Actual time: +20 minutes
+"""
+
+from programming_language import ProgrammingLanguage
+
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+
+
+main()
---
+++ prac_06/programming_language.py
@@ -0,0 +1,27 @@
+"""
+CP1404/CP5632 Practical
+Intermediate Exercise
+Estimated time: 35 minutes
+Actual time:
+"""
+
+class ProgrammingLanguage:
+
+    def __init__(self, name="", typing="", reflection=True, year=""):
+
+        self.name = name
+        self.typing = typing
+        if self.is_dynamic():
+            self.typing = "Dynamic"
+        else:
+            self.typing = "Static"
+        self.reflection = reflection
+        self.year = year
+
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
+
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
