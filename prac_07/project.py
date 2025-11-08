bc06e18381d231255aa53418919ec1fc0f30863c
Saxonn <saxonncapilos1@gmail.com>
2025-11-08 12:58:53 +1000

    Create menu function for project management exercise.

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,6 +1,21 @@
 """
 CP1404 - Prac_07
 Do-from-scratch Exercises - Project Management Program
-Estimated time: 60 minutes
+Estimated time: 240 minutes
 Actual time:
 """
+
+
+class Project:
+    """Represent a project with [INSERT ATTRIBUTES] along with other required functions."""
+
+    def __init__(self, name="", start_date="", priority=1, cost_estimate=0, completion_perecentage=0):
+        """Construct a project from the given values."""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_perecentage
+
+
+    def
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,7 +1,50 @@
 """
 CP1404 - Prac_07
 Do-from-scratch Exercises - Project Management Program
-Estimated time: 60 minutes
+Estimated time: 240 minutes
 Actual time:
 """
+
+import datetime
+MENU = ("- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new project\n"
+        "-(U)pdate project\n- (Q)uit")
+
+def main():
+    """Run project_management program."""
+    print("Welcome to Pythonic Project Management")
+    get_choice()
+
+
+def retrieve_projects(in_file):
+    """Return each line of projects.txt as a list of separated strings."""
+    book_entries = [line.strip().split(',') for line in in_file]
+    return book_entries
+
+
+
+def get_choice():
+    """Display menu and get choice from user."""
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            print("load_projects()")
+        elif choice == "S":
+            print("save_projects()")
+        elif choice == "D":
+            print("display_projects()")
+        elif choice == "F":
+            print("filter_projects()")
+        elif choice == "A":
+            print("add_new_project()")
+        elif choice == "U":
+            print("update_project()")
+        else:
+            print("Invalid menu choice.")
+        choice = input(">>> ").upper()
+
+
+
+if __name__ == "__main__":
+    main()
 
