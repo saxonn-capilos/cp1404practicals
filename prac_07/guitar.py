61f394d3ea878e98ca438e3b140998beb87f379f
Saxonn <saxonncapilos1@gmail.com>
2025-11-10 11:55:07 +1000

    Copied formatting

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -1,13 +1,11 @@
 """
-CP1404/CP5632 Prac_06
-Do-From-Scratch Exercise
-Estimated time: minutes
-Actual time:  minutes
+CP1404/CP5632 Prac_07
+Intermediate Exercise
 """
 
 
 class Guitar:
-
+    """Represent a guitar with name, year, and cost along with other required functions."""
     def __init__(self, name="", year=0, cost=0):
         """Construct a Guitar from the given values."""
         self.name = name
@@ -28,7 +26,6 @@ class Guitar:
     def get_age(self):
         """Return how old the guitar is in years (e.g. in 2025 the L-5 is: 2025 - 1922 = 103)."""
         return 2025 - self.year
-
 
     def is_vintage(self):
         """Returns True if the Guitar is 50 or more years old, False otherwise."""
